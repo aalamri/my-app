8e676625074f03c0ff06d63dff34be85754e4c0f
dalba1992 <hans.sven315@gmail.com>
2020-05-29 19:35:21 +0800

    toast integrated

--- frontend/src/public_site/components/Signup/index.js
+++ frontend/src/public_site/components/Signup/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import { setToken } from "../../../utils/index";
 import Strapi from "strapi-sdk-javascript/build/main";
+import { Toast } from 'react-bootstrap';
 
 const emailUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:1337";
 const url = process.env.REACT_APP_BACKEND_URL + "/auth/local/register";
@@ -16,7 +17,9 @@ class Signup extends React.Component {
     password: "12345678",
     loading: false,
     successmessage: false,
-    seconds: 10
+    seconds: 10,
+    toastShow: false,
+    errorMessage: ""
   };
 
   handleChange = (event) => {
@@ -42,32 +45,65 @@ class Signup extends React.Component {
       console.log("Signup.handleSubmit");
     }
 
-    try {
-      this.setState({ loading: true });
-      const response = await axios({
-        method: "POST",
-        url,
-        data,
-      });
-      // await strapi.request('POST', '/email', {
-      //   data: {
-      //     to: email,
-      //     subject: `Request New User - Modrek ${new Date(Date.now())}`,
-      //     text: "Your reuqest has been processed",
-      //     html: "<bold>Expect your request to approve 1-2 days</bold>"
-      //   }
-      // });
+    fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data),
+    }).then(resp => resp.json().then(res => {
       this.setState({ loading: false });
-      setToken(response.data.jwt);
-      this.timer = setInterval(() => {
-        if (this.state.seconds === 1) { this.props.history.push("/") }
-        this.setState({ seconds: --this.state.seconds })
-      }, 1000);
-      // this.redirectUser("/");
-      this.setState({ successmessage: true });
-    } catch (err) {
+      if (res.jwt) {
+        setToken(res.jwt);
+        this.timer = setInterval(() => {
+          if (this.state.seconds === 1) { this.props.history.push("/") }
+          this.setState({ seconds: --this.state.seconds })
+        }, 1000);
+        this.setState({ successmessage: true });
+      } else {
+        this.setState({ errorMessage: res.data[0].messages[0].message, toastShow: true });
+      }
+    }).catch(err => {
       this.setState({ loading: false });
-    }
+      console.log('err', err)
+    })).catch(err => err.json().then(error => {
+      this.setState({ loading: false });
+
+      console.log(error)
+    }));
+    // axios.post(url, data).then(response => {
+    //   console.log('response', response);
+    // }).catch(error => {
+    //   console.log('error', error)
+    // })
+
+    // try {
+    //   this.setState({ loading: true });
+    //   const response =  axios({
+    //     method: "POST",
+    //     url,
+    //     data,
+    //   });
+    //   // await strapi.request('POST', '/email', {
+    //   //   data: {
+    //   //     to: email,
+    //   //     subject: `Request New User - Modrek ${new Date(Date.now())}`,
+    //   //     text: "Your reuqest has been processed",
+    //   //     html: "<bold>Expect your request to approve 1-2 days</bold>"
+    //   //   }
+    //   // });
+    //   this.setState({ loading: false });
+    //   setToken(response.data.jwt);
+    //   this.timer = setInterval(() => {
+    //     if (this.state.seconds === 1) { this.props.history.push("/") }
+    //     this.setState({ seconds: --this.state.seconds })
+    //   }, 1000);
+    //   // this.redirectUser("/");
+    //   this.setState({ successmessage: true, toastShow: true });
+    // } catch (err) {
+    //   this.setState({ loading: false, toastShow: true });
+    // }
   };
 
   redirectUser = (path) => this.props.history.push(path);
@@ -184,12 +220,28 @@ class Signup extends React.Component {
               </div>
             </div>
           </div>
+
         </section> :
           <div className={"container text-center"}>
             <h1 className="section-title mt-5">Thank you for applying to us</h1>
             <h3 className="mt-2 mb-5">An admin will contact you on your email to proccess your request</h3>
             <div className={"mt-5 testlist-name"}>you’ll be directed to home in {this.state.seconds} secounds</div>
           </div>}
+
+        <Toast
+          style={{
+            position: 'absolute',
+            top: 30,
+            right: 20,
+            zIndex: 3000
+          }}
+          onClose={() => this.setState({ toastShow: false })} show={this.state.toastShow} delay={3000} autohide
+        >
+          <Toast.Header>
+            <strong className="mr-auto">Error</strong>
+          </Toast.Header>
+          <Toast.Body>{this.state.errorMessage}</Toast.Body>
+        </Toast>
       </>
     );
   }
