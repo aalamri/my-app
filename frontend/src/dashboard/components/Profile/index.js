d0513e01431ffbf96e7c1150b48ab1fc78a357aa
aalamri <ali.alamri.987@gmail.com>
2020-06-17 22:19:59 +0300

    change style in profile

--- frontend/src/dashboard/components/Profile/index.js
+++ frontend/src/dashboard/components/Profile/index.js
@@ -39,21 +39,35 @@ class Profile extends React.Component {
     render() {
         const { email, lastName, firstName } = this.state;
         return (
-            <div className={"dashboard-profile"}>
-                <div className={"row action-row"}>
-                    <Button variant="warning" className={"change-password"} onClick={() => { this.changePassword() }}>Change Password</Button>
-                    <Button variant="secondary" onClick={() => { this.deleteAccount() }}>Delete Account</Button>
+            <div className="dashboard-profile">
+            <div className={"row action-row"}>
+            <Button variant="warning" className={"change-password"} onClick={() => { this.changePassword() }}>Change Password</Button>
+            <Button variant="secondary" onClick={() => { this.deleteAccount() }}>Delete Account</Button>
+             </div>
+                <section className="max-width-880 mx-auto pt-100 mb-5">
+                <div className="container">
+                  <div className="row">
+                  <div className="col-md-6">
+                  <div className="form-group tajawal">
+                       <label className="profile-label">First Name: </label>
+                <p type="text">{this.state.user.lastName}</p>
                 </div>
-                <div className="uk-child-width-1-2" data-uk-grid dir="auto">
-                    First Name: {this.state.user.lastName}
                 </div>
-                <div className="uk-child-width-1-2" data-uk-grid dir="auto">
-                    Last Name: {this.state.user.firstName}
+                <div className="col-md-6">
+                <div className="form-group tajawal">
+                <label className="profile-label">Last Name: </label>
+                <p>{this.state.user.firstName}</p>
+                  </div>
                 </div>
-                <div className="uk-child-width-1-2" data-uk-grid dir="auto">
-                    Email: {this.state.user.email}
+                <div className="col-md-6">
+                <div className="form-group tajawal">
+                <label className="profile-label">Email: </label>
+                <p>{this.state.user.email}</p>
+                  </div>
                 </div>
-
+                </div>
+                </div>
+                </section>
                 <Modal show={this.state.passwordModal} onHide={() => { this.handleClose() }}>
                     <Modal.Header closeButton>
                         <Modal.Title>Change Password</Modal.Title>
